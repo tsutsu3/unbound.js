b97930208f0625d47fe1898d52df481e6d563257
tsutsu3 <tsutsu3prog@gmail.com>
2024-12-01 14:07:42 +0900

    Add tls test

--- tests/control.snapshot.test.ts
+++ tests/control.snapshot.test.ts
@@ -1,7 +1,8 @@
-import { UnixUnboundClient } from "../src/index";
 import fs from "fs";
 import path from "path";
 import YAML from "yaml";
+import { UnixUnboundClient, TcpUnboundClient } from "../src/index";
+import { TlsConfig } from "../src/types";
 
 const baseDir = path.resolve(__dirname);
 const unboundVersion = process.env.UNBOUND_VERSION || "1.22.0";
@@ -58,7 +59,65 @@ describe(`Unix domain socket docker server tests. Unbound version: ${unboundVers
             : [];
 
         try {
-          // メソッド呼び出し
+          const result = await method.apply(client, args); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+          expect(result).toMatchSnapshot();
+        } catch (error) {
+          expect((error as Error).message).toMatchSnapshot();
+        } finally {
+          await client.disconnect();
+        }
+      });
+    }
+  }
+});
+
+describe(`TCP socket docker server tests. Unbound version: ${unboundVersion}`, () => {
+  let client: TcpUnboundClient;
+  const keyPath = path.join(
+    baseDir,
+    "../unbound-config/tls/key/unbound_control.key",
+  );
+  const certPath = path.join(
+    baseDir,
+    "../unbound-config/tls/key/unbound_control.pem",
+  );
+  const tlsConfig: TlsConfig = {
+    cert: certPath,
+    key: keyPath,
+  };
+
+  beforeAll(() => {
+    client = new TcpUnboundClient("localhost", 8953, tlsConfig);
+  });
+
+  const files = fs
+    .readdirSync(dataDir)
+    .filter((file) => file.endsWith(".yaml"));
+
+  for (const file of files) {
+    const command = file.replace(".yaml", "");
+    const fileContent = fs.readFileSync(path.join(dataDir, file), "utf-8");
+    const contents = YAML.parse(fileContent) as ParsedData;
+
+    for (const { title, options } of contents.data) {
+      it(`${command} test: ${title}`, async () => {
+        console.log(`Running ${command} test: ${title}`);
+
+        const method = client[command as keyof UnixUnboundClient].bind(
+          client,
+        ) as (...args: any[]) => Promise<any>; // eslint-disable-line @typescript-eslint/no-explicit-any
+        if (typeof method !== "function") {
+          throw new Error(`Invalid command: ${command}`);
+        }
+
+        const args =
+          options !== undefined
+            ? Array.isArray(options)
+              ? options
+              : [options]
+            : [];
+
+        try {
           const result = await method.apply(client, args); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
           expect(result).toMatchSnapshot();
         } catch (error) {
